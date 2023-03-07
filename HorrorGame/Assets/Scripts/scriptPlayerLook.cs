01ab064850d4491cf5eb302a74d36a2c3976c539
samlaconico <99830026+samlaconico@users.noreply.github.com>
2023-03-06 18:22:41 -0800

    updated movement

--- HorrorGame/Assets/Scripts/scriptPlayerLook.cs
+++ HorrorGame/Assets/Scripts/scriptPlayerLook.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ public class scriptPlayerLook : MonoBehaviour
 {
     public float mouseSens = 100f;
     public Transform playerBody;
-
+    public Canvas ui;
     private float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
@@ -25,5 +26,27 @@ public class scriptPlayerLook : MonoBehaviour
         
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, 2);
+        
+        if (hit && hitInfo.transform.gameObject.tag == "interactable")
+        {
+            ui.enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                interact(hitInfo);
+                
+            }
+        }
+        else
+        {
+            ui.enabled = false;
+        }
+    }
+
+    private void interact(RaycastHit hitInfo)
+    {
+        hitInfo.transform.gameObject.GetComponent<MeshRenderer>().material.color = new Color(.3f, .3f, .6f);
     }
 }
